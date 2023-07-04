42316ce2e915d7230ba0d5309aefe9e75bd32a5e
nixon100 <nixon6637@gmail.com>
2023-07-04 07:21:14 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import circle from './components/circle';
+
+
+function App() {
+  return (
+    <div className="App">
+     <circle />
+     hi
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/circle.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function circle(){
+    return(
+        <div className="circle">
+            circle
+        </div>
+    )
+}
